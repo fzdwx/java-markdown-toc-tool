053084fa20b8c784c54b97a2ab66cc8d43ef5f3d
李可 <65269574+likedeke@users.noreply.github.com>
2021-08-03 07:45:37 +0800

    Update MdTocToolCliArg.java

--- src/main/java/org/atomicoke/mdtoc/MdTocToolCliArg.java
+++ src/main/java/org/atomicoke/mdtoc/MdTocToolCliArg.java
@@ -12,8 +12,8 @@ public class MdTocToolCliArg {
 
     @Parameter(names = {"--version", "-v"})
     public boolean version;
-    @Parameter(names = {"--localFile", "-lf"}, description = "解析本地的MarkDown文件 \n\tParse the local MarkDown file,like D:\\Java\\project\\markdown-toc-tool\\README.md")
+    @Parameter(names = {"--localFile", "-lf"}, description = "解析本地的MarkDown文件 \nParse the local MarkDown file,like D:\\Java\\project\\markdown-toc-tool\\README.md")
     public String localFile;
-    @Parameter(names = {"--remoteFile", "-rf"}, description = "解析远程的MarkDown文件 \n\tParse remote MarkDown file,like https://gitee.com/atomic-coke/like-back-end/raw/like/README.md")
+    @Parameter(names = {"--remoteFile", "-rf"}, description = "解析远程的MarkDown文件 \nParse remote MarkDown file,like https://gitee.com/atomic-coke/like-back-end/raw/like/README.md")
     public String remoteFile;
 }
